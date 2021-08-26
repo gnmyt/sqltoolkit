5680c1968f502900a5a1e85b516c612ab7b5ad1f
mathias <germannewsmaker@gmail.com>
2021-08-26 16:20:59 +0200

    Added the query parameter enum

---
+++ src/main/java/de/gnmyt/sqltoolkit/querybuilder/QueryParameter.java
@@ -0,0 +1,34 @@
+package de.gnmyt.sqltoolkit.querybuilder;
+
+/**
+ * All usable query parameters for the {@link QueryBuilder}
+ */
+public enum QueryParameter {
+
+    /**
+     * The name of the table provided as a {@link String}
+     */
+    TABLE_NAME,
+
+    /**
+     * All 'where'-parameters of the query provided as a {@link java.util.HashMap}
+     */
+    WHERE_LIST,
+
+    /**
+     * All values of the query provided as a {@link java.util.HashMap}
+     */
+    VALUE_LIST,
+
+    /**
+     * All 'set'-parameters of the query provided as a {@link java.util.HashMap}
+     */
+    SET_LIST,
+
+    /**
+     * The row limit of a query provided as a {@link String}
+     */
+    LIMIT
+
+
+}
